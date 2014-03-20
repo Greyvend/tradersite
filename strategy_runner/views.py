6db56cfc0db3eb2ee8082ac24b909ec0813a05c7
Serge Mosin <svmosin@gmail.com>
2014-03-20 19:05:34 +0700

    Minor adjustment.

--- strategy_runner/views.py
+++ strategy_runner/views.py
@@ -113,38 +113,3 @@ def run(request):
 class LongPeriodException(Exception):
     def __str__(self):
         return "The chosen period is too long to form a graph"
-
-
-
-
-
-
-
-def example(request):
-    return render(request, 'strategy_runner/example.html')
-
-import random
-from django.http import HttpResponse
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-from matplotlib.dates import DateFormatter
-
-
-def simple(request):
-    fig = Figure()
-    ax = fig.add_subplot(111)
-    x = []
-    y = []
-    now = datetime.datetime.now()
-    delta = datetime.timedelta(days=1)
-    for i in range(10):
-        x.append(now)
-        now += delta
-        y.append(random.randint(0, 1000))
-    ax.plot_date(x, y, '-')
-    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
-    fig.autofmt_xdate()
-    canvas = FigureCanvas(fig)
-    response = HttpResponse(content_type='image/png')
-    canvas.print_png(response)
-    return response
